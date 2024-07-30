ee3162f2ca5a4965d000bfb43fd9b882f1cc981a
Carlos <morales22@outlook.com>
2024-07-30 14:37:40 -0500

    Initial Commit

---
+++ fetchMealApp/fetchMealAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchMealAppApp.swift
+//  fetchMealApp
+//
+//  Created by Carlos Morales III on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct fetchMealAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
